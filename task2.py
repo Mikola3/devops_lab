c8fe61d86d2c0bb1e114a3db19e0240c86bf3dc5
pavelkislouski <Pavel_Kislouski@epam.com>
2018-03-06 12:36:14 -0500

    task2_1

--- task2.py
+++ task2.py
@@ -6,9 +6,12 @@ matrix = [ list(map(int, input().split())) for i in range(N)]
 
 sumMain = 0
 sumSecondary = 0
+dif = 0 
 for i in range(N):
     sumMain += matrix[i][i]
     sumSecondary += matrix[i][N-i-1]
+dif = sumMain - sumSecondary
 
 print(sumMain)
 print(sumSecondary)
+print(dif)
