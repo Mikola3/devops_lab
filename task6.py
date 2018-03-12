5cc7b27e2ee441dd4c92af90b3ce6eadfd29e69f
pavelkislouski <Pavel_Kislouski@epam.com>
2018-03-12 18:58:01 -0400

    code checkers

--- task6.py
+++ task6.py
@@ -4,74 +4,80 @@ import os
 import json
 import yaml
 
-vp = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2])
+a1 = sys.version_info[0]
+a2 = sys.version_info[1]
+a3 = sys.version_info[2]
+vp = str(a1) + "." + str(a2) + "." + str(a3)
+
 
 def write_to_json():
-	d1={}
-	d1["Version"]=vp
-	os.system("pyenv virtualenvs > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Virtual Environment"]=content
-	d1["Python Executable Location"]=sys.executable
-	os.system("which pip > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Pip Location"]=content
-	d1["PYTHONPATH"]=str((os.environ['PATH'].split(os.pathsep))[0])
-	os.system("pip list > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Installed Packages"]=content
+    d1 = {}
+    d1["Version"] = vp
+    os.system("pyenv virtualenvs > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Virtual Environment"] = content
+    d1["Python Executable Location"] = sys.executable
+    os.system("which pip > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Pip Location"] = content
+    d1["PYTHONPATH"] = str((os.environ['PATH'].split(os.pathsep))[0])
+    os.system("pip list > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Installed Packages"] = content
 
-	i = 0
-	while i < len(sys.path):
-		if 'site-packages' in sys.path[i]:
-			d1["Site-Packages Location "]=sys.path[i]
-			print(sys.path[i])
-		i += 1
-		
-	# open json file task6.json & write in it
-	with open('task6.json', 'a') as myfile: 
-		json.dump(d1, myfile, indent = 4, ensure_ascii = False)
-	return 1
+    i = 0
+    while i < len(sys.path):
+        if 'site-packages' in sys.path[i]:
+            d1["Site-Packages Location "] = sys.path[i]
+            print(sys.path[i])
+        i += 1
+
+    # open json file task6.json & write in it
+    with open('task6.json', 'a') as myfile:
+        json.dump(d1, myfile, indent=4, ensure_ascii=False)
+    return 1
+
 
 def write_to_yaml():
-	d1={}
-	d1["Version"]=vp
-	os.system("pyenv virtualenvs > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Virtual Environment"]=content
-	d1["Python Executable Location"]=sys.executable
-	os.system("which pip > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Pip Location"]=content
-	d1["PYTHONPATH"]=str((os.environ['PATH'].split(os.pathsep))[0])
-	os.system("pip list > specilFile.txt")
-	f = open("specilFile.txt", 'r')
-	content = f.read()
-	f.close()
-	d1["Installed Packages"]=content
-	i = 0
-	while i < len(sys.path):
-		if 'site-packages' in sys.path[i]:
-			d1["Site-Packages Location "]=sys.path[i]
-			print(sys.path[i])
-		i += 1
-		
-	# open yaml file task6.yaml & write in it
-	with open('task6.yaml', 'a') as myfile: 
-		yaml.dump(d1, myfile, default_flow_style=False, allow_unicode=True)
-	return True
+    d1 = {}
+    d1["Version"] = vp
+    os.system("pyenv virtualenvs > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Virtual Environment"] = content
+    d1["Python Executable Location"] = sys.executable
+    os.system("which pip > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Pip Location"] = content
+    d1["PYTHONPATH"] = str((os.environ['PATH'].split(os.pathsep))[0])
+    os.system("pip list > specilFile.txt")
+    f = open("specilFile.txt", 'r')
+    content = f.read()
+    f.close()
+    d1["Installed Packages"] = content
+    i = 0
+    while i < len(sys.path):
+        if 'site-packages' in sys.path[i]:
+            d1["Site-Packages Location "] = sys.path[i]
+            print(sys.path[i])
+        i += 1
+
+    # open yaml file task6.yaml & write in it
+    with open('task6.yaml', 'a') as myfile:
+        yaml.dump(d1, myfile, default_flow_style=False, allow_unicode=True)
+    return True
+
 
 if __name__ == "__main__":
-	# execute only if run as a script
-	write_to_json()
-	write_to_yaml()
+    # execute only if run as a script
+    write_to_json()
+    write_to_yaml()
--- test_task6.py
+++ test_task6.py
@@ -1,19 +1,19 @@
 from unittest import TestCase
-
 import task6
+
 
 class TestPrime(TestCase):
 
-  def setUp(self):
-    """Init"""
+    def setUp(self):
+        """Init"""
 
-  def test_is_json(self):
-    """Test for json"""
-    self.assertTrue(task6.write_to_json())
-    
-  def test_is_yaml(self):
-    """Test for yaml"""
-    self.assertTrue(task6.write_to_yaml())
+    def test_is_json(self):
+        """Test for json"""
+        self.assertTrue(task6.write_to_json())
 
-  def tearDown(self):
-    """Finish"""
+    def test_is_yaml(self):
+        """Test for yaml"""
+        self.assertTrue(task6.write_to_yaml())
+
+    def tearDown(self):
+        """Finish"""
