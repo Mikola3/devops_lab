c0eafca6272fa685754c83614e4c402dfe18fc8f
pavelkislouski <Pavel_Kislouski@epam.com>
2018-03-12 13:51:27 -0400

    task6_2

--- task6.py
+++ task6.py
@@ -4,29 +4,7 @@ import os
 import json
 import yaml
 
-print("Version's Python is ", str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2]))
 vp = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2])
-
-print ("Virtual Environment:")
-os.system("pyenv virtualenvs")
-
-print("Python Executable Location ", sys.executable)
-
-print("Pip Location:")
-os.system("which pip")
-
-print ("PYTHONPATH:")
-print(str((os.environ['PATH'].split(os.pathsep))[0]))
-
-print ("Installed Packages: Name, Version:")
-os.system("pip list")
-
-i = 0
-while i < len(sys.path):
-	if 'site-packages' in sys.path[i]:
-		print("Site-Packages Location ")
-		print(sys.path[i])
-	i += 1
 
 def write_to_json():
 	d1={}
@@ -35,8 +13,8 @@ def write_to_json():
 	f = open("specilFile.txt", 'r')
 	content = f.read()
 	f.close()
-	d1["VE"]=content
-	d1["PEL"]=sys.executable
+	d1["Virtual Environment"]=content
+	d1["Python Executable Location"]=sys.executable
 	os.system("which pip > specilFile.txt")
 	f = open("specilFile.txt", 'r')
 	content = f.read()
@@ -59,6 +37,7 @@ def write_to_json():
 	# open json file task6.json & write in it
 	with open('task6.json', 'a') as myfile: 
 		json.dump(d1, myfile, indent = 4, ensure_ascii = False)
+	return 1
 
 def write_to_yaml():
 	d1={}
@@ -67,8 +46,8 @@ def write_to_yaml():
 	f = open("specilFile.txt", 'r')
 	content = f.read()
 	f.close()
-	d1["VE"]=content
-	d1["PEL"]=sys.executable
+	d1["Virtual Environment"]=content
+	d1["Python Executable Location"]=sys.executable
 	os.system("which pip > specilFile.txt")
 	f = open("specilFile.txt", 'r')
 	content = f.read()
@@ -90,6 +69,9 @@ def write_to_yaml():
 	# open yaml file task6.yaml & write in it
 	with open('task6.yaml', 'a') as myfile: 
 		yaml.dump(d1, myfile, default_flow_style=False, allow_unicode=True)
+	return True
 
-write_to_json()
-write_to_yaml()
+if __name__ == "__main__":
+	# execute only if run as a script
+	write_to_json()
+	write_to_yaml()
